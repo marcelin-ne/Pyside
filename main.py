586f0e920fc67699f270d5369049dc8ea7f6daa8
Henry Moya <mmontalvo@sodigsa.com>
2023-10-20 17:30:22 -0500

    add comments to explain the script

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+#importing the component we need
 from PySide6.QtWidgets import QApplication, QWidget
 
+#SYS: Procces comand line arguments to pick up by QAPP
 import sys
 
+#run the aplicattion and wait the thing to happend
 app=QApplication(sys.argv)
 
+#window are hidden by default
 window=QWidget()
+#show the hidden window
 window.show()
-
-app.exec()
+#waiting the things to happend  (Event loop catching)
+app.exec()  #Start de event loop
+#app.exec_() #This will be removed , do not use
